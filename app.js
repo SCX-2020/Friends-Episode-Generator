57c2c9162b246a5b36993dd0f0aab28fb9112d28
SCX-2020 <69284866+SCX-2020@users.noreply.github.com>
2023-05-03 18:16:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 function getRandomEpisode() {
-  const season = Math.floor(Math.random() * 10) + 1;
-  const episode = Math.floor(Math.random() * 24) + 1;
-  const result = document.getElementById("result");
-  result.innerHTML = `Season ${season}, Episode ${episode}`;
+    // Generate a random number between 1 and 236
+    const episodeNumber = Math.floor(Math.random() * 236) + 1;
+
+    // Redirect the user to the randomly generated episode
+    window.location.href = `https://www.netflix.com/watch/70153404?trackId=${episodeNumber}`;
 }
 
 document.getElementById("random-episode-button").addEventListener("click", getRandomEpisode);
