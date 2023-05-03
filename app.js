edd598ebe891bf1940ea810dee5637c52a42bd28
SCX-2020 <69284866+SCX-2020@users.noreply.github.com>
2023-05-03 18:33:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-function getRandomEpisode() {
-    // Generate a random number between 1 and 236
-    const episodeNumber = Math.floor(Math.random() * 236) + 1;
+const episodes = [
+  {
+    season: 1,
+    episode: 1,
+    netflixId: '70272489'
+  },
+  {
+    season: 1,
+    episode: 2,
+    netflixId: '70272490'
+  },
+  {
+    season: 1,
+    episode: 3,
+    netflixId: '70272491'
+  },
+  // ... 其他季数和集数
+  {
+    season: 10,
+    episode: 16,
+    netflixId: '70272645'
+  }
+];
 
-    // Redirect the user to the randomly generated episode
-    window.location.href = `https://www.netflix.com/watch/70153404?trackId=${episodeNumber}`;
+function getRandomEpisode() {
+  const randomIndex = Math.floor(Math.random() * episodes.length);
+  const episode = episodes[randomIndex];
+
+  const netflixUrl = `https://www.netflix.com/watch/${episode.netflixId}`;
+  const episodeTitle = `Season ${episode.season}, Episode ${episode.episode}`;
+  const episodeInfo = `Watch "${episodeTitle}" on <a href="${netflixUrl}" target="_blank">Netflix</a>`;
+
+  document.getElementById('episode-info').innerHTML = episodeInfo;
 }
 
-document.getElementById("random-episode-button").addEventListener("click", getRandomEpisode);
+document.getElementById('random-episode-button').addEventListener('click', getRandomEpisode);
